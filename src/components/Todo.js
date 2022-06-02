249b419c781bbe44aeb967556d80e1fb05478bd0
phonhay103 <phonhay103@gmail.com>
2022-06-02 09:21:42 +0000

    Change Todo status

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
 
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
-
 /* コンポーネント */
 import TodoItem from './TodoItem';
 import Input from './Input';
@@ -26,6 +19,16 @@ function Todo() {
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
+  
+  const handleCheck = checked => {
+    const newItems = items.map(item => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  };
 
   return (
     <div className="panel">
@@ -33,9 +36,10 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       {items.map(item => (
-        <TodoItem
+        <TodoItem 
           key={item.key}
           item={item}
+          onCheck={handleCheck}
         />
       ))}
       <div className="panel-block">
