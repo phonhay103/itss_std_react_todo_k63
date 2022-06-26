2da2a73eb337ad368f993f538cb3c0099802e399
phonhay103 <phonhay103@gmail.com>
2022-06-26 15:31:09 +0000

    Save Todo on Firestore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,8 +3,6 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,3 +17,51 @@ const firebaseConfig = {
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
+
+/////////////////////////////////////////////
+const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const addFirebaseItem = async (item) => {
+  try {
+    const todoRef = db.collection("todos");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const updateFirebaseItem = async (item, id) => {
+  try {
+    const todoRef = db.collection("todos").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const clearFirebaseItem = async (item) => {
+  const todoRef = db.collection("todos").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+};
+
+//////////////////////////////////////////////////////////
